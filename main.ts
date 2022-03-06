2ca3cce30626ada117f6e506a67a6d0f740ee184
Filipko <98056125+FilipKopecny@users.noreply.github.com>
2022-03-06 18:36:36 +0100

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,8 +2,9 @@ radio.onReceivedString(function (receivedString) {
     if (receivedString == "snimaj") {
         for (let index = 0; index < pocet_merani; index++) {
             pins.servoWritePin(AnalogPin.P16, uhol)
-            basic.pause(100)
+            basic.pause(20)
             radio.sendValue("uhol", uhol)
+            basic.pause(20)
             radio.sendValue("dialka", sonar.ping(
             DigitalPin.P14,
             DigitalPin.P15,
@@ -11,6 +12,7 @@ radio.onReceivedString(function (receivedString) {
             ))
             uhol += 180 / pocet_merani
         }
+        basic.pause(500)
         uhol = 0
         pins.servoWritePin(AnalogPin.P16, uhol)
     }
@@ -21,4 +23,4 @@ radio.setGroup(1)
 uhol = 0
 pins.servoWritePin(AnalogPin.P16, uhol)
 let dialka = 0
-pocet_merani = 20
+pocet_merani = 30
