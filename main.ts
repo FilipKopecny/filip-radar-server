fd0b6c95a2af8d2dacf600962c25378184c2975a
Filipko <98056125+FilipKopecny@users.noreply.github.com>
2022-03-06 18:20:09 +0100

    Počiatočné súbory pre projekt MakeCode

---
+++ main.ts
@@ -0,0 +1,24 @@
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "snimaj") {
+        for (let index = 0; index < pocet_merani; index++) {
+            pins.servoWritePin(AnalogPin.P16, uhol)
+            basic.pause(100)
+            radio.sendValue("uhol", uhol)
+            radio.sendValue("dialka", sonar.ping(
+            DigitalPin.P14,
+            DigitalPin.P15,
+            PingUnit.Centimeters
+            ))
+            uhol += 180 / pocet_merani
+        }
+        uhol = 0
+        pins.servoWritePin(AnalogPin.P16, uhol)
+    }
+})
+let pocet_merani = 0
+let uhol = 0
+radio.setGroup(1)
+uhol = 0
+pins.servoWritePin(AnalogPin.P16, uhol)
+let dialka = 0
+pocet_merani = 20
